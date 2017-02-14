607c988384a2c0b0cfecc44c55c96b3da16befb9
MJ <munkh60gal.mn@gmail.com>
2017-02-14 10:52:42 -0800

    Create linked-list-cycle.py

---
+++ linked-list-cycle.py
@@ -0,0 +1,16 @@
+#https://leetcode.com/problems/linked-list-cycle/
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                return True
+        return False
