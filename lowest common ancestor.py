f1793427257d6584ad2a6d96d4ad530abf9fba8f
MJ <munkh60gal.mn@gmail.com>
2017-02-14 16:11:38 -0800

    Create lowest common ancestor.py

---
+++ lowest common ancestor.py
@@ -0,0 +1,7 @@
+# lowest common ancestor
+
+def lowestCommonAncestor(self, root, p, q):
+    if root in (None, p, q): return root
+    left, right = (self.lowestCommonAncestor(kid, p, q)
+                   for kid in (root.left, root.right))
+    return root if left and right else left or right
