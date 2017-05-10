572671d4bc73be1cedb407657f0d09ee3a1878cf
MJ <munkh60gal.mn@gmail.com>
2017-05-10 14:54:47 -0700

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
+#define BOOL bool
+
+bool isNumericChar(char s){
+  return (s >= '0' && s <= '9')? true:false;
+}
 
 int main(){
   int a = 0, i;
-  char str[] = "234";
-  //printf("%c", str[1]);
-  //printf("%d", str[1]);
+  char str[] = "2dr";
   for( i=0 ; i<strlen(str) ; i++){
-    if (48<=str[i]<=57){
-      a = a*10 + str[i]-48;
+    if (isNumericChar(str[i])==true){
+
+      a = a*10 + str[i]-'0';
     }
-    printf("%d\n", a);    
+  
   }
+  printf("%d\n", a);   
 }
+
+
